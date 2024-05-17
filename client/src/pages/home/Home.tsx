9cdcc010728c1cf2cae2997173a0475406209073
QuantumRiddle <samurai-1-02@mail.ru>
2024-05-17 18:41:07 +0300

    Add: styles in linkBrand.scss

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,9 +1,13 @@
 import { TextModule } from "@src/shared/scss"
+import { LinkBrand } from "@src/shared/ui/link"
+import Test_Img from "@src/shared/assets/Test/Samsung_logo.png"
+import linkBrand from "@src/shared/ui/link/link-brand/linkBrand.module.scss"
 
 export const Home = () => {
   return (
     <div>
         <h1 className={TextModule.p18}>Hello</h1>
+        <LinkBrand to = "/"><img  className = {linkBrand.brand_img} src={Test_Img} alt="" /></LinkBrand>
     </div>
   )
 }
--- client/src/shared/ui/link/link-brand/LinkBrand.tsx
+++ client/src/shared/ui/link/link-brand/LinkBrand.tsx
@@ -1,7 +1,13 @@
-export const LinkBrand = () => {
-    return (
-        <div>
+import {FC} from "react";
+import {Link} from "react-router-dom";
+import {ILinkBase} from "@src/shared/ui/link/type/LinkType";
+import linkBrand from "@src/shared/ui/link/link-brand/linkBrand.module.scss"
 
-        </div>
+export const LinkBrand: FC<ILinkBase> = (props) => {
+    const {to, children, className} = props;
+    return (
+        <Link to={to} className={linkBrand.brand_card}>
+            {children}
+        </Link>
     );
 };
